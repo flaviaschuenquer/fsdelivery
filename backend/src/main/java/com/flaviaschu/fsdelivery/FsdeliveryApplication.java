d77c66a8d9b458bbcebc74c540fc48686df89add
Flaiva Schuenquer <flavia_schuenquer@hotmail.com>
2021-12-08 15:23:54 -0300

    Project created

---
+++ backend/src/main/java/com/flaviaschu/fsdelivery/FsdeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.flaviaschu.fsdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsdeliveryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/flaviaschu/fsdelivery/FsdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flaviaschu.fsdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
