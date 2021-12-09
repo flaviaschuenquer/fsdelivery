a809c7deb12a9a8b8f0956e72d14c3d792d2fc72
Flaiva Schuenquer <flavia_schuenquer@hotmail.com>
2021-12-08 23:05:39 -0300

    GET orders

---
+++ backend/src/main/java/com/flaviaschu/fsdelivery/services/OrderService.java
@@ -0,0 +1,28 @@
+package com.flaviaschu.fsdelivery.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.flaviaschu.fsdelivery.dto.OrderDTO;
+import com.flaviaschu.fsdelivery.entities.Order;
+import com.flaviaschu.fsdelivery.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> finAll(){
+		List<Order> list = repository.findAll();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+		
+	}
+	
+	
+}
