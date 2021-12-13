9ca963df603626c142e1a803d72ee3100dc23483
Flaiva Schuenquer <flavia_schuenquer@hotmail.com>
2021-12-12 22:22:08 -0300

    Add orders list

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -6,8 +6,8 @@ import {
 import AppLoading from 'expo-app-loading'
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import Header from './src/Header'
+import { StyleSheet, View } from 'react-native'
+import Routes from './src/Routes'
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -18,20 +18,16 @@ export default function App() {
   if (!fontsLoaded) {
     return <AppLoading />
   }
-
   return (
     <View style={styles.container}>
-      <Header />
       <StatusBar style="auto" />
+      <Routes />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    flex: 1
   }
 })
